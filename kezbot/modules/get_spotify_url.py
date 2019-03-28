keessonnema/kezbot

commit a6c6d99941765919e7df8b5bd592c51aaed3cf72

modules: Disable webpage previews
Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

kezbot/modules/get_spotify_url.py
@@ -79,7 +79,7 @@ def get_sp_url(_bot, update):
                                 spot_title = spot_tracks[0]['name']
                                 spot_url = spot_tracks[0]['external_urls']['spotify']
                                 update.effective_message.reply_text \
-                                    ("► {0} - {1} \n{2}".format(spot_artist, spot_title, spot_url))
+                                    ("► {0} - {1} \n{2}".format(spot_artist, spot_title, spot_url), disable_web_page_preview=True)
                             else:
                                 if update.message.chat.type == "private":
                                     update.effective_message.reply_text \

kezbot/modules/get_youtube_url.py
@@ -39,7 +39,7 @@ def get_yt_url(_bot, update):
                     get_video_id = re.sub("'", '', video_info['items'][0]['id']['videoId'])
                     video_url = ("https://youtube.com/watch?v={0}".format(get_video_id))
                     update.effective_message.reply_text \
-                        ("► {0} - {1} \n{2}".format(artist, track, video_url))
+                        ("► {0} - {1} \n{2}".format(artist, track, video_url), disable_web_page_preview=True)
                 else:
                     print('No videoId!')
             else:

commit b4ab16f044031da342221b578eb368502be19fea

Fix missing parameter in spotify module
Missed it in a6c6d9994176

Fixes: a6c6d9994176 ("modules: Disable webpage previews")
Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

kezbot/modules/get_spotify_url.py
@@ -69,7 +69,7 @@ def get_sp_url(_bot, update):
                             spot_title = spot_tracks[0]['name']
                             spot_url = spot_tracks[0]['external_urls']['spotify']
                             update.effective_message.reply_text \
-                                ("► {0} - {1} \n{2}".format(spot_artist, spot_title, spot_url))
+                                ("► {0} - {1} \n{2}".format(spot_artist, spot_title, spot_url), disable_web_page_preview=True)
                         else:
                             results = spotify.search(q="artist:{} track:{}".format(track, artist, limit=1))
                             spot_tracks = results['tracks']['items']